5888649dfd506e513e427d883de022e1f443adce
yangiksoon <yangiksoon@gmail.com>
2021-01-16 15:31:23 +0900

    key update

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,19 @@
 resource "aws_instance" "ec2_yang" {
+  subnet_id     = "aws_subnet.sbn_yang_public1.ids"
+
   ami           = "ami-0e67aff698cb24c1d" # ap-northeast-2 Ubuntu 18.04
+  associate_public_ip_address = true
   instance_type = "t2.micro"
   vpc_security_group_ids = [
       "sg-1436abcf",
   ]
   tags          = {
     Name        = "EC2-YANG-JENKINS"
-    Environment = "production"
+    Environment = "prod"
   }
    root_block_device {
-    delete_on_termination = false
+    delete_on_termination = true
+    volume_size = "8GiB"
   }
+  key_name = "aws_key_pair.key_yang.KEY-YANG"
 }
---
+++ key.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "key_yang" {
+  key_name   = "KEY-YANG"
+  public_key = "/home/yangiksoon/.ssh/id_rsa.pub"
+}
